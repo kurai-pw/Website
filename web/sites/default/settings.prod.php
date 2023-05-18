d052b17c8935e53e478ccad0cf20420d6743cef0
hzswdef <ostelia.group@gmail.com>
2023-05-18 20:19:01 +0300

    hhhhhhhhhhhhhhhhh

--- web/sites/default/settings.prod.php
+++ web/sites/default/settings.prod.php
@@ -1,4 +1,7 @@
 <?php
 
+$settings['reverse_proxy'] = TRUE;
 $settings['reverse_proxy_addresses'] = ['new.kurai.pw'];
 $settings['reverse_proxy_host_header'] = 'X_FORWARDED_HOST';
+
+$base_url = 'https://new.kurai.pw/';
